ae2dbb47bae42a1e2fb403d75bd41ddac4a1550a
Nischal Shrestha <nischalstha9@gmail.com>
2023-03-10 08:41:59 +0545

    ask on palindrome

--- palindrome.java
+++ palindrome.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter text to check:");
         String name = sc.nextLine();
         sc.close();
         String newname = "";
