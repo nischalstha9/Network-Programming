b41a56373250f9247a77a55e00b156408d1307a3
Nischal Shrestha <nischalstha9@gmail.com>
2023-05-03 21:06:18 +0545

    ssl

--- SSL/CipherSuitesMethods.java
+++ SSL/CipherSuitesMethods.java
@@ -1,5 +1,6 @@
 package SSL;
 
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -8,13 +9,29 @@ public class CipherSuitesMethods {
     public static void main(String[] args) {
         try {
             SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            SSLSocket sso = (SSLSocket) ssf.createSocket();
-            sso.connect(new InetSocketAddress("localhost", 8000));
+            SSLSocket sso = (SSLSocket) ssf.createSocket("google.com", 80);
+
             System.out.println((sso.getEnabledCipherSuites()));
-            System.out.println(sso.getSupportedCipherSuites());
+            String[] cipherSuites = sso.getSupportedCipherSuites();
+            System.out.println(cipherSuites);
             System.out.println(sso.getRemoteSocketAddress());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
+// public class CipherSuitesMethods {
+// public static void main(String[] args) {
+// try {
+// SSLSocketFactory ssf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+// SSLSocket sso = (SSLSocket) ssf.createSocket();
+// sso.connect(new InetSocketAddress("localhost", 8000));
+// System.out.println((sso.getEnabledCipherSuites()));
+// System.out.println(sso.getSupportedCipherSuites());
+// System.out.println(sso.getRemoteSocketAddress());
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
+// }
